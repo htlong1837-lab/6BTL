95ea6563f3fe1454164f4bde65fcd73456edbea6
knganoiiw <kimngan12011978@gmail.com>
2026-03-30 07:10:50 +0700

    :)))

---
+++ Ngân/Role.java
@@ -0,0 +1,9 @@
+package Ngân;
+
+// Enum (enumeration) là một kiểu dữ liệu đặc biệt dùng để định nghĩa tập hợp các hằng số cố định, có tên.
+public enum Role {
+    BIDDER,   // người đấu giá
+    SELLER,   // người bán
+    ADMIN     // quản trị viên
+    
+}
