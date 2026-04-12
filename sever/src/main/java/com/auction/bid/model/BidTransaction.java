60ab3740cacb250f5f64e566d7f3bd29e1d8c11a
longming8377-cell <longming8377@gmail.com>
2026-04-12 14:44:28 +0700

    p.1.1
    thêm bid

--- sever/src/main/java/com/auction/bid/model/BidTransaction.java
+++ sever/src/main/java/com/auction/bid/model/BidTransaction.java
@@ -1,5 +1,31 @@
 package com.auction.bid.model;
 
 public class BidTransaction {
+    private String bidderId;
+    private String bidderName;
+    private String auctionId;
+    private double amount;
+    private long timestamp;
+    public BidTransaction(String bidderId, String bidderName, String auctionID, double amount, long timestamp) {
+        this.bidderId = bidderId;
+        this.bidderName = bidderName;
+        this.auctionId = auctionID;
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+    public String getBidderId() {
+        return bidderId;
     
+    }
+    public String getBidderName() {
+        return bidderName;
+    }
+    public String getAuctionId()  { return auctionId; }
+    public double getAmount()     { return amount; }
+    public long getTimestamp()    { return timestamp; }
+    @Override
+    public String toString() {
+        return "Bid[bidder=" + bidderName + ", amount=" + amount + ", time=" + timestamp + "]";
+    }
+                
 }
--- sever/src/main/java/com/auction/item/model/Electronics.java
+++ sever/src/main/java/com/auction/item/model/Electronics.java
@@ -1,5 +1,30 @@
 package com.auction.item.model;
 
-public class Electronics {
-    
+public class Electronics extends Item {
+    private String brand;
+    private int warrantyMonths;
+    public Electronics(String id, String name, String des, double startPrice, String category, String sellerId, String brand, int warrantyMonths) {
+        super(id, name, des, startPrice, category, sellerId);
+        this.brand = brand;
+        this.warrantyMonths = warrantyMonths;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public int getWarrantyMonths() {
+        return warrantyMonths;
+
+    }
+    public void setBrand(String a) {
+        this.brand = a;
+    }
+    public void setWarrantyMonths(int b) {
+        this.warrantyMonths = b;
+    }
+    @Override
+    public void printInfo() {
+        super.printInfo(); 
+        System.out.println("Brand :" + brand);
+        System.out.println("WarrantyMonths :" + warrantyMonths);
+    }
 }
