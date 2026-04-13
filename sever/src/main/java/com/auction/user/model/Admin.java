07c99bd39c934c6fe06f4cb50190e72ca6b06af7
ynipham <nhitranginpham@gmail.com>
2026-04-13 18:27:41 +0700

    update

--- sever/src/main/java/com/auction/user/model/Admin.java
+++ sever/src/main/java/com/auction/user/model/Admin.java
@@ -13,6 +13,41 @@ public class Admin extends User {
         this.adminLevel = level;
 
     }
+    /** Xóa một phiên đấu giá khỏi danh sách */
+    public void removeAuction(List<Auction> auctions, Auction target) {
+        if (auctions.remove(target)) {
+            System.out.println("[Admin] Auction for \"" + target.getItem().getName() + "\" has been removed.");
+        } else {
+            System.out.println("[Admin] Auction not found.");
+        }
+    }
+ 
+    /** Khóa tài khoản người dùng (ví dụ khi vi phạm) */
+    public void banUser(User user) {
+        System.out.println("[Admin] User \"" + user.getName() + "\" has been banned.");
+    // Mở lại tài khoản người dùng
+    public void isActive(User user) {
+        System.out.println("[Admin] User \"" + user.getName() + "\" has been activated. You can now access");
+    }
+    // Duyệt sản phẩm
+    public void approveItem(Item item) {
+        if (item.isApproved()) {
+            System.out.println("[Admin] Item \"" + item.getName() + "\" is already approved. It's ready for auction right now");
+        } else {
+            System.out.println("[Admin] Item \"" + item.getName() + "\" is invalid. Please check the details and edit it for approval.");
+        }
+    }
+    // Ghi nhận hành vi đánh giá bất thường
+    public void flagSuspiciousActivity(User user) {
+        if (!user.login(false)){
+            int failedLogin = user.login(false)+1;
+            if (failedLogin >= 3) {
+                banUser(user);
+                System.out.println("[Admin] User \"" + user.getName() + "\" has been flagged for suspicious activity due to multiple failed login attempts.");
+            } else {
+                System.out.println("[Admin] User \"" + user.getName() + "\" has a failed login attempt. Total failed attempts: " + failedLogin);
+            }
+        }
     @Override
     public void printInfo() {
         super.printInfo();
