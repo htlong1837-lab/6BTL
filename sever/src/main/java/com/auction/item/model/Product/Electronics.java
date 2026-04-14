4d4f7cbbc11e7431ecd1b368fce933f4fd53a3c6
longming8377-cell <longming8377@gmail.com>
2026-04-14 16:22:00 +0700

    qưeqưe
    ưqeqưq

--- sever/src/main/java/com/auction/item/model/Product/Electronics.java
+++ sever/src/main/java/com/auction/item/model/Product/Electronics.java
@@ -1,38 +1,26 @@
 package com.auction.item.model.Product;
 
-import java.lang.module.ModuleDescriptor.Builder;
-
-class Electronics extends Item {
+public class Electronics extends Item {
     private String brand;
     private int warrantyMonths;
-    public Electronics(Builder builder) {
-        super(builder.id, builder.name, builder.des, builder.startPrice, builder.category, builder.sellerId);
-        this.brand = builder.brand;
-        this.warrantyMonths = builder.warrantyMonths;
-    }s
 
-<<<<<<< HEAD
-    //getter
-=======
-
->>>>>>> 9667fe2d5aa0b49693bd10022a7d11ca678ffc1f
-    public String getBrand() {
-        return brand;
-    }
-    public int getWarrantyMonths() {
-        return warrantyMonths;
+    public Electronics(String id, String name, String des, double startPrice,
+                       String category, String sellerId, String brand, int warrantyMonths) {
+        super(id, name, des, startPrice, category, sellerId);
+        this.brand = brand;
+        this.warrantyMonths = warrantyMonths;
     }
 
-    //setter
-    void setBrand(String a) {
-            this.brand = a;
-    }
-        
-    void setWarrantyMonths(int b) {
-        this.warrantyMonths = b;
-    }
+    public String getBrand() { return brand; }
+    public int getWarrantyMonths() { return warrantyMonths; }
 
-    
-  
+    public void setBrand(String brand) { this.brand = brand; }
+    public void setWarrantyMonths(int warrantyMonths) { this.warrantyMonths = warrantyMonths; }
 
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Brand    : " + brand);
+        System.out.println("Warranty : " + warrantyMonths + " months");
+    }
 }
