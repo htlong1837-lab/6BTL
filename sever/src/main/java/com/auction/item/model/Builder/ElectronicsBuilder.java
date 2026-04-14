b5813affde48fe4aaa809eafa47addb0682bfe8f
longming8377-cell <longming8377@gmail.com>
2026-04-14 18:03:13 +0700

    11
    11

--- sever/src/main/java/com/auction/item/model/Builder/ElectronicsBuilder.java
+++ sever/src/main/java/com/auction/item/model/Builder/ElectronicsBuilder.java
@@ -1,43 +1,67 @@
 package com.auction.item.model.Builder;
 
+import com.auction.item.model.Product.Electronics;
 import com.auction.item.model.Product.Item;
 
+public class ElectronicsBuilder implements ItemBuilder {
 
-public class ElectronicsBuilder implements ItemBuilder{
-    private Electronics item = new Electronics();
+    private String id;
     private String name;
     private String des;
     private double startPrice;
+    private String category;
+    private String sellerId;
     private String brand;
     private int warrantyMonths;
 
-    public ItemBuilder setName(String name) {
+    @Override
+    public ElectronicsBuilder setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public ElectronicsBuilder setName(String name) {
         this.name = name;
         return this;
     }
-    public ItemBuilder setDes(String des) {
+
+    @Override
+    public ElectronicsBuilder setDes(String des) {
         this.des = des;
         return this;
     }
 
-    public ItemBuilder setStartPrice(double startPrice) {
+    @Override
+    public ElectronicsBuilder setStartPrice(double startPrice) {
         this.startPrice = startPrice;
         return this;
     }
 
-    public ItemBuilder setBrand(String a) {
-        this.brand = a;
-        return this;
-    }
-    
-    public ItemBuilder setWarrantyMonths(int b) {
-        this.warrantyMonths = b;
+    @Override
+    public ElectronicsBuilder setCategory(String category) {
+        this.category = category;
         return this;
     }
 
-    public Item build(){
-        return item;
+    @Override
+    public ElectronicsBuilder setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+        return this;
     }
 
+    public ElectronicsBuilder setBrand(String brand) {
+        this.brand = brand;
+        return this;
+    }
 
+    public ElectronicsBuilder setWarrantyMonths(int warrantyMonths) {
+        this.warrantyMonths = warrantyMonths;
+        return this;
+    }
+
+    @Override
+    public Item build() {
+        return new Electronics(id, name, des, startPrice, category, sellerId, brand, warrantyMonths);
+    }
 }
--- sever/src/main/java/com/auction/item/model/Builder/ItemBuilder.java
+++ sever/src/main/java/com/auction/item/model/Builder/ItemBuilder.java
@@ -1,9 +1,13 @@
 package com.auction.item.model.Builder;
 
+import com.auction.item.model.Product.Item;
+
 public interface ItemBuilder {
+    ItemBuilder setId(String id);
     ItemBuilder setName(String name);
     ItemBuilder setDes(String des);
     ItemBuilder setStartPrice(double price);
-    ItemBuilder setSellerId(String sellerid);
-    
+    ItemBuilder setCategory(String category);
+    ItemBuilder setSellerId(String sellerId);
+    Item build();
 }
