7114374f3fd50a6acd98f631522ed2faa98b6e5d
longming8377-cell <longming8377@gmail.com>
2026-04-21 16:07:12 +0700

    2eqưe
    ưeqưe

--- sever/src/main/java/com/auction/bid/controller/BidController.java
+++ sever/src/main/java/com/auction/bid/controller/BidController.java
@@ -1,5 +1,28 @@
 package com.auction.bid.controller;
-
+import com.auction.bid.service.BidService;
+import com.auction.user.model.Bidder;
 public class BidController {
+    private final BidService bidService;
+    public BidController(BidService bidService) {
+        this.bidService = bidService;
+    }
+    public String handlePlaceBid(Bidder bidder, String auctionId, double amount) {
+        return bidService.placeBid(bidder,auctionId,amount);
+    }
+    public String handleDeposit(Bidder bidder, double amount) {
+        return bidService.deposit(bidder, amount);
+
+    }
+    public String handleWithdraw(Bidder bidder, String auctionId) {
+        return bidService.withdrawBid(bidder, auctionId);
+    }
+    public String handleViewHistory(Bidder bidder) {
+        return bidService.viewBidHistory(bidder);
+    }
+    public String handleViewActive(Bidder bidder) {
+        return bidService.viewActiveBids(bidder);
+    }
+
+
     
 }
