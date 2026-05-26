b147256e6c65ac379430d85baabffd29ec8a5d13
longming8377-cell <longming8377@gmail.com>
2026-05-26 17:35:22 +0700

    last last update
    11

--- run-client.bat
+++ run-client.bat
@@ -1,4 +1,16 @@
 @echo off
+if not exist "%~dp0Client\target\client.jar" (
+    echo Chua co client.jar, dang build...
+    cd /d "%~dp0Client"
+    call mvn package -DskipTests -q
+    if %errorlevel% neq 0 (
+        echo.
+        echo Build that bai. Bam phim bat ky de dong...
+        pause >nul
+        exit /b 1
+    )
+    echo Build thanh cong!
+)
 java -jar "%~dp0Client\target\client.jar"
 if %errorlevel% neq 0 (
     echo.
